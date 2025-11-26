02c8912f0524834f359a43273ec2452714c20a66
Yosr Frikha <yosr.frikha1@gmail.com>
2025-11-26 22:47:57 +0100

    models pour signaler un problème +TSDoc

---
+++ libs/shared/data-access/src/lib/models/signalement.model.ts
@@ -0,0 +1,26 @@
+import { TypeDechet } from "../enums";
+import { GeoPoint } from "../interfaces";
+
+/**
+ * DTO pour signaler un conteneur endommagé
+ */
+export interface SignalerEndommageDto {
+  localisation: GeoPoint;
+  typeDechet: TypeDechet;
+}
+
+/**
+ * DTO pour signaler un véhicule en panne
+ */
+export interface SignalerVehiculePanneDto {
+  matricule: string;
+  typeDechet: TypeDechet;
+  localisation: GeoPoint;
+}
+
+/**
+ * DTO pour signaler un incident sur le trajet
+ */
+export interface SignalerIncidentDto {
+  localisation: GeoPoint;
+}
