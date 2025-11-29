52b4807d7fd12479930c2fe0fd64da7be83a11c4
Yosr Frikha <yosr.frikha1@gmail.com>
2025-11-29 10:38:40 +0100

    exporter les interfaces de shared data access

--- libs/shared/data-access/src/index.ts
+++ libs/shared/data-access/src/index.ts
@@ -1,1 +1,8 @@
+/**
+ * Barrel export pour la librairie shared/data-access
+ * Exporte tous les modèles et interfaces
+ */
+export * from './lib/models';
+export * from './lib/interfaces';
+export * from './lib/enums';
 
