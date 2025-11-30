767f40564856f2b49ec84caf28141129caf0f8fd
Yosr Frikha <yosr.frikha1@gmail.com>
2025-11-30 21:53:36 +0100

    indexes update

---
+++ libs/shared/ui/src/lib/components/index.ts
@@ -0,0 +1,5 @@
+export * from './confirm-dialog/confirm-dialog.component';
+export * from './data-table/data-table.component';
+export * from './layout';
+export * from './leaflet-map/leaflet-map';
+export * from './stat-card/stat-card.component';
--- libs/shared/ui/src/lib/components/layout/index.ts
+++ libs/shared/ui/src/lib/components/layout/index.ts
@@ -1,2 +1,3 @@
 export * from './header/header.component';
 export * from './sidebar/sidebar.component';
+export * from './main-layout/main-layout.component';
