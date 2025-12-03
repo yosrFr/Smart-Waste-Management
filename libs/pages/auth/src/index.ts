28d150746344cd73ebf71deaec8395c4a87acca5
Yosr Frikha <yosr.frikha1@gmail.com>
2025-12-04 00:19:17 +0100

    indexes update

--- libs/pages/auth/src/index.ts
+++ libs/pages/auth/src/index.ts
@@ -1,1 +1,6 @@
 export * from './lib/login/login.component';
+export * from './lib/change-password/change-password.component';
+export * from './lib/guards/auth.guard';
+export * from './lib/guards/admin.guard';
+export * from './lib/guards/employee.guard';
+export * from './lib/auth.routes';
---
+++ libs/pages/shared/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/notifications/notifications.component';
+export * from './lib/profil/profil.component';
+export * from './lib/shared.routes';
