476f162279f7118b1bb19b192d019e0981eac58b
Yosr Frikha <yosr.frikha1@gmail.com>
2025-12-04 22:11:57 +0100

    update leaflet component

--- libs/shared/ui/src/lib/components/index.ts
+++ libs/shared/ui/src/lib/components/index.ts
@@ -9,3 +9,4 @@ export * from './loading-spinner/loading-spinner.component';
 export * from './empty-state/empty-state.component';
 export * from './status-badge/status-badge.component';
 export * from './tabs/tabs.component';
+export * from './map-chooser-dialog/map-chooser-dialog.component';
