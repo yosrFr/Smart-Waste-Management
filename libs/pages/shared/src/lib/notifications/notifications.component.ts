3d6d9c781b0b5bfc471ed77ccf91a5ca9a5418f0
Yosr Frikha <yosr.frikha1@gmail.com>
2025-12-09 22:29:36 +0100

    update, fix and tsdoc notifications page

--- libs/pages/shared/src/lib/notifications/notifications.component.ts
+++ libs/pages/shared/src/lib/notifications/notifications.component.ts
@@ -7,15 +7,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { Store } from '@ngrx/store';
-import { Observable, take } from 'rxjs';
+import { Observable } from 'rxjs';
 import {
   selectPaginatedNotifications,
   selectAllNotifications,
   selectNotificationPageIndex,
-  selectNotificationPageSize,
   selectNotificationTotalPages,
   setPageIndex,
-  setPageSize,
   AppNotification,
   TypeNotif,
   selectNotificationsLoading,
@@ -57,16 +55,32 @@ import {
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.css',
 })
+/**
+ * Page Liste des notifications
+ * Affiche toutes les notifications avec pagination, icônes et détails contextuels selon leur type.
+ */
 export class NotificationsComponent implements OnInit {
   private store = inject(Store);
 
+  /** Notifications paginées */
   paginatedNotifications$: Observable<AppNotification[]>;
+
+  /** Toutes les notifications */
   allNotifications$: Observable<AppNotification[]>;
+
+  /** Index de page actuel */
   pageIndex$: Observable<number>;
-  pageSize$: Observable<number>;
+
+  /** Nombre total de pages */
   totalPages$: Observable<number>;
+
+  /** Indique si les notifications sont en cours de chargement */
   loading$: Observable<boolean>;
+
   TypeNotif = TypeNotif;
+
+  /** Taille fixe d'une page */
+  pageSize = 10;
 
   constructor() {
     this.paginatedNotifications$ = this.store.select(
@@ -74,33 +88,26 @@ export class NotificationsComponent implements OnInit {
     );
     this.allNotifications$ = this.store.select(selectAllNotifications);
     this.pageIndex$ = this.store.select(selectNotificationPageIndex);
-    this.pageSize$ = this.store.select(selectNotificationPageSize);
     this.totalPages$ = this.store.select(selectNotificationTotalPages);
     this.loading$ = this.store.select(selectNotificationsLoading);
   }
 
+  /** Charge les notifications au loading du composant */
   ngOnInit(): void {
-    // Charge les notifications au montage du composant
     this.store.dispatch(loadNotifications());
   }
 
   /**
    * Gère le changement de page
+   * @param event Evénement du paginator Material
    */
   onPageChange(event: PageEvent): void {
     this.store.dispatch(setPageIndex({ pageIndex: event.pageIndex }));
-    this.store
-      .select(selectNotificationPageSize)
-      .pipe(take(1))
-      .subscribe((currentSize) => {
-        if (event.pageSize !== currentSize) {
-          this.store.dispatch(setPageSize({ pageSize: event.pageSize }));
-        }
-      });
   }
 
   /**
    * Retourne l'icône pour un type de notification
+   * @param type Type de notification
    */
   getNotificationIcon(type: TypeNotif): string {
     const icons: Record<TypeNotif, string> = {
@@ -115,11 +122,19 @@ export class NotificationsComponent implements OnInit {
 
   /**
    * Retourne la classe CSS pour un type de notification
+   * @param type Type de notification
    */
   getNotificationClass(type: TypeNotif): string {
     return type.toLowerCase().replace(/_/g, '-');
   }
 
+  /**
+   * Prend une notification de type inconnu
+   * Cherche le type de notification
+   * Retourne les détails de cette notification selon son type
+   * @param notification une notification reçue qu'on ne connait pas son type
+   * @returns Les détails d'un type spécifique de notification
+   */
   getNotificationDetails(notification: AppNotification) {
     switch (notification.type) {
       case TypeNotif.PLEIN:
@@ -138,6 +153,11 @@ export class NotificationsComponent implements OnInit {
     }
   }
 
+  /**
+   * Type guard : vérifie si la notification concerne un conteneur
+   * @param notification notification reçue qu'on ne connait pas son type
+   * @returns True si la notification de type plein ou endommagé, sinon retourne 0
+   */
   isConteneur(
     notification: AppNotification
   ): notification is
@@ -149,18 +169,33 @@ export class NotificationsComponent implements OnInit {
     );
   }
 
+  /**
+   * Type guard : notification liée à un véhicule
+   * @param notification notification reçue qu'on ne connait pas son type
+   * @returns True si la notification de type panne, sinon retourne 0
+   */
   isVehicule(
     notification: AppNotification
   ): notification is NotificationVehiculeEnPanne {
     return notification.type === TypeNotif.PANNE;
   }
 
+  /**
+   * Type guard : notification liée à un Incident
+   * @param notification notification reçue qu'on ne connait pas son type
+   * @returns True si la notification de type incident, sinon retourne 0
+   */
   isIncident(
     notification: AppNotification
   ): notification is NotificationIncident {
     return notification.type === TypeNotif.INCIDENT;
   }
 
+  /**
+   * Type guard : notification liée à une tâche
+   * @param notification notification reçue qu'on ne connait pas son type
+   * @returns True si la notification de type nouvelle tâche, sinon retourne 0
+   */
   isNouvelleTache(
     notification: AppNotification
   ): notification is NotificationNouvelleTache {
