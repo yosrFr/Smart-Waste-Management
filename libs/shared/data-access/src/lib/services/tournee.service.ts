f22fb268e978a43e5d1033cd4af3f9b1d44d698e
Yosr Frikha <yosr.frikha1@gmail.com>
2025-12-12 22:47:19 +0100

    tournee service type date update

--- libs/shared/data-access/src/lib/services/tournee.service.ts
+++ libs/shared/data-access/src/lib/services/tournee.service.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @nx/enforce-module-boundaries */
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
 import { Tournee } from '../models';
 import { HttpClient } from '@angular/common/http';
 
@@ -13,13 +13,21 @@ import { HttpClient } from '@angular/common/http';
 export class TourneeService {
   private apiUrl = '/api/tournees';
 
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
   /**
    * Récupère toutes les tournées
    * @returns Observable avec la liste des tournées
    */
   getAll(): Observable<Tournee[]> {
-    return this.http.get<Tournee[]>(`${this.apiUrl}/all`);
+    return this.http.get<Tournee[]>(`${this.apiUrl}/all`).pipe(
+      map((data) =>
+        data.map((t) => ({
+          ...t,
+          dateDebut: new Date(t.dateDebut),
+          dateFin: new Date(t.dateFin),
+        }))
+      )
+    );
   }
 }
